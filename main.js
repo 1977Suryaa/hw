8073089622ffd398c6aae4cfdfe4ff17a63bfeff
1977Suryaa <101641242+1977Suryaa@users.noreply.github.com>
2022-10-07 15:01:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,28 @@
 
+
 function setup() {
   createCanvas(400,400);
   webcam=createCapture(VIDEO)
   webcam.hide()
+
  
   
   
 }
 
 function draw(){
-    
+    fill("green")
     circle(30, 30, 50);
     circle(350, 30, 50);
     circle(30, 350, 50);
     circle(350, 350, 50);
+    fill("red")
     rect(20, 55, 20, 270);
     rect(340, 55, 20, 270);
     rect(55, 20, 270, 20);
     rect(55, 340, 270, 20);
-    image(capture, 0, 0, width, width * capture.height / capture.width);
+
+    image(webcam,100,100,200,200)
     
 
 
